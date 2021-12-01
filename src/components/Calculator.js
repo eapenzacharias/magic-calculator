9808578ad336add1bdc56a360aab2ed41dd8c950
eapenzacharias <eapenzac@gmail.com>
2021-12-01 19:46:21 +0100

    Added calculate function to the components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,27 +2,40 @@ import React from 'react';
 import Display from './Display';
 import RenderCol from './ButtonPanel';
 import Button from './Button';
+import calculate from '../logic/calculate';
 import './style/style.scss';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: '0',
+      next: null,
+      // eslint-disable-next-line react/no-unused-state
+      operations: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const cal = this.state;
+    this.setState(calculate(cal, buttonName));
   }
 
   render() {
+    const { total, next } = this.state;
     return (
       <div className="container main">
-        <Display />
+        <Display value={next || total || '0'} />
         <div className="button-panel">
-          <RenderCol btn0="A" btn1="+/-" btn2="%" btn3="÷" />
-          <RenderCol btn0="7" btn1="8" btn2="9" btn3="x" />
-          <RenderCol btn0="4" btn1="5" btn2="6" btn3="-" />
-          <RenderCol btn0="1" btn1="2" btn2="3" btn3="+" />
+          <RenderCol clickHandler={this.handleClick} btn0="AC" btn1="+/-" btn2="%" btn3="÷" />
+          <RenderCol clickHandler={this.handleClick} btn0="7" btn1="8" btn2="9" btn3="x" />
+          <RenderCol clickHandler={this.handleClick} btn0="4" btn1="5" btn2="6" btn3="-" />
+          <RenderCol clickHandler={this.handleClick} btn0="1" btn1="2" btn2="3" btn3="+" />
           <div className="btn-col">
-            <Button buttonName="0" wide="wide" color="grey" />
-            <Button buttonName="." color="grey" />
-            <Button buttonName="=" />
+            <Button clickHandler={this.handleClick} buttonName="0" wide="wide" color="grey" />
+            <Button clickHandler={this.handleClick} buttonName="." color="grey" />
+            <Button clickHandler={this.handleClick} buttonName="=" />
           </div>
         </div>
       </div>

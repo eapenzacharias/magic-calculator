8c193557910f423b58d1778cb212fed4b0933f1e
eapenzacharias <eapenzac@gmail.com>
2021-12-08 22:59:04 +0100

    updated router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Router basename={process.env.PUBLIC_UR} L>
+        <Router>
           <AppContainer />
         </Router>
       </div>

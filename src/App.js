0dfc20058aa49297c02b5f693ab35fd571552972
eapenzacharias <eapenzac@gmail.com>
2021-12-08 17:21:35 +0100

    added homepage and basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_UR} L>
           <AppContainer />
         </Router>
       </div>
